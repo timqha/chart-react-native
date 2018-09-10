ea04ea95925e1663ae4ce5048db8e3a5bd3e6f59
Olexander Tymoshenko <otymoshenko@s-pro.io>
2018-09-10 18:25:43 +0300

    feature(chart): add curve chart.

--- App.js
+++ App.js
@@ -8,204 +8,209 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Dimensions
+  Dimensions,
+  PanResponder,
+  ART,
+  Animated
 } from 'react-native';
-import { LineChart as LNKit } from 'react-native-chart-kit';
-import PureChart from 'react-native-pure-chart';
-import LineChartDemo from "react-native-responsive-linechart";
-import {
-  LineChart,
-  Grid,
-  Path,
-  YAxis
-} from 'react-native-svg-charts';
-import {
-  Circle,
-  G,
-  Line,
-  Text,
-  Rect
-} from 'react-native-svg';
-/* eslint-disable-next-line */
-import * as shape from 'd3-shape';
+import Svg, { Path, Circle, G, Line, Rect, Text } from 'react-native-svg';
 
+import * as shape from 'd3-shape';
+import * as scale from 'd3-scale';
+
+
+const COLORS = {
+  BG: '#595cab',
+  BASE_LINE: '#cf46a0',
+  SHADOW: '#7151a7',
+  DARK_LINE: '#534895',
+  WHITE: '#eeecf4',
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    alignItems: 'flex-start',
+    backgroundColor: COLORS.WHITE,
   },
 });
-
+const { width, height } = Dimensions.get('window');
 
 type Props = {};
 export default class App extends Component<Props> {
-  componentDidMount() {
 
+  state = {
+    itemIndex: 1
   }
 
-  render() {
-    const data = [50, 70, 100, 138, 100, 70, 50];
+  linspace = 0;
 
-    const Shadow = ({ line }) => (
-      <Path
-        key="shadow"
-        y={2}
-        d={line}
-        fill="none"
-        strokeWidth={16}
-        stroke="rgba(134, 65, 244, 0.2)"
-      />
-    );
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+      
+      onPanResponderMove: (evt, gs) => {
+        this.setState(() => ({
+          itemIndex: Math.ceil(gs.moveX / this.linspace)
+        }));
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming
+        // the JS responder. Returns true by default. Is currently only supported on
+        // android.
+        return true;
+      }
+    })
+  }
+  
+  createPaths = ({ data, x, y }) => {
+    const line = shape.line()
+      .x(d => x(d.x))
+      .y(d => y(d.y))
+      .defined(item => typeof item.y === 'number')
+      // for linear curveLinear
+      .curve(shape.curveNatural)(data);
+    return {
+      path: line,
+      line,
+    };
+  }
+
+  xAccessor = ({ index }) => index;
+
+  yAccessor = ({ item }) => item;
+
+
+  render() {
+
+    const data = [50, 60, 70, 80, 90, 110, 50, 40];
+    this.linspace = width / data.length;
+    const offset = 5;
+    const maxOfDataY = 110 + offset + 100;
+    const maxOfDataX = data.length-1;
+
+    const {
+      gridMax,
+      gridMin,
+      clampX,
+      clampY,
+      svg,
+    } = this.props;
+
+
+    const mappedData = data.map((item, index) => ({
+      y: this.yAccessor({ item, index }),
+      x: this.xAccessor({ item, index }),
+    }));
+
+
+    // invert range to support svg coordinate system
+    const y = scale.scaleLinear()
+      .domain([0, maxOfDataY])
+      .range([height / 2, offset])
+      .clamp(clampY);
+
+    const x = scale.scaleLinear()
+      .domain([0, maxOfDataX])
+      .range([offset, width - 0])
+      .clamp(false);
+
+    const paths = this.createPaths({
+      data: mappedData,
+      x,
+      y,
+    });
+
+    const extraProps = {
+      x,
+      y,
+      data,
+
+      width,
+      height,
+      ...paths,
+  }
 
 
     const Tooltip = ({ x, y }) => {
-      const position = 75;
-      const itemIndex = 2;
-      return (
+      console.log(x(0))
+      console.log(y(data[0]))
+      const {itemIndex} = this.state;
+      console.log('itemIndex', itemIndex);
+      if (itemIndex && itemIndex < data.length - 1) {
+        return (
         <G
-          x={x(itemIndex) - (position / 2)}
-          key="tooltip"
-          onPress={() => console.log('tooltip clicked')}
+            x={ x(itemIndex) - (75 / 2) }
+            key={ 'tooltip' }
+            onPress={ () => console.log('tooltip clicked') }
         >
-          <G y={50}>
-            <Rect
-              height={40}
-              width={75}
-              stroke="grey"
-              fill="white"
-              ry={10}
-              rx={10}
-            />
-            <Text
-              x={position / 2}
-              dy={20}
-              alignmentBaseline="middle"
-              textAnchor="middle"
-              stroke="rgb(134, 65, 244)"
-            >
-              { `${data[itemIndex]} KG` }
-            </Text>
-          </G>
-          <G x={position / 2}>
-            <Line
-              y1={50 + 40}
-              y2={y(data[itemIndex])}
-              stroke="grey"
-              strokeWidth={2}
-            />
-            <Circle
-              cy={y(data[itemIndex])}
-              r={6}
-              stroke="rgb(134, 65, 244)"
-              strokeWidth={2}
-              fill="white"
-            />
-          </G>
+            <G y={ 50 }>
+                <Rect
+                    height={ 40 }
+                    width={ 75 }
+                    stroke={ 'grey' }
+                    fill={ 'white' }
+                    ry={ 10 }
+                    rx={ 10 }
+                />
+                <Text
+                    x={ 75 / 2 }
+                    dy={ 20 }
+                    alignmentBaseline={ 'middle' }
+                    textAnchor={ 'middle' }
+                    stroke={ 'rgb(134, 65, 244)' }
+                >
+                    { `${data[itemIndex]} KG` }
+                </Text>
+            </G>
+            <G x={ 75 / 2 }>
+                <Line
+                    y1={ 50 + 40 }
+                    y2={ y(data[ itemIndex ]) }
+                    stroke={ 'grey' }
+                    strokeWidth={ 2 }
+                />
+                <Circle
+                    cy={ y(data[ itemIndex ]) }
+                    r={ 6 }
+                    stroke={ 'rgb(134, 65, 244)' }
+                    strokeWidth={ 2 }
+                    fill={ 'white' }
+                />
+            </G>
         </G>
-      );
-    };
+    )
+      } else {
+        return null
+      }
+}
+
     return (
       <View style={styles.container}>
-        <LNKit
-          data={{
-            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-            datasets: [{
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100
-              ]
-            }]
-          }}
-          width={Dimensions.get('window').width} // from react-native
-          height={220}
-          chartConfig={{
-            backgroundColor: '#e26a00',
-            backgroundGradientFrom: '#fb8c00',
-            backgroundGradientTo: '#ffa726',
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
-              borderRadius: 16
-            }
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
-            borderRadius: 16
-          }}
-        />
-
-        <PureChart data={[30, 200, 170, 250, 10]} type="line" />
-
-        <LineChartDemo
-          style={{ flex: 1 }}
-          config={{
-            line: {
-              strokeWidth: 1,
-              strokeColor: "#216D99"
-            },
-            area: {
-              gradientFrom: "#2e86de",
-              gradientFromOpacity: 1,
-              gradientTo: "#87D3FF",
-              gradientToOpacity: 1
-            },
-            yAxis: {
-              labelColor: "#c8d6e5"
-            },
-            grid: {
-              strokeColor: "#c8d6e5",
-              stepSize: 30
-            },
-            insetY: 10,
-            insetX: 10,
-            interpolation: "spline",
-            backgroundColor: "#fff"
-          }}
-          data={[-10, -15, 40, 19, 32, 15, 52, 55, 20, 60, 78, 42, 56]}
-        />
-
-        <YAxis
-          data={data}
-          style={{ marginLeft: 10, marginBottom: -80, height: '45%' }}
-          contentInset={{ top: 10, bottom: 10 }}
-          svg={{ fontSize: 10, fill: 'grey' }}
-        />
-        <LineChart
-
-          style={{ height: '55%', width: Dimensions.get('window').width }}
-          data={data}
-          svg={{ stroke: 'rgb(134, 65, 244)', strokeWidth: 3, strokeOpacity: 0.9 }}
-          contentInset={{ top: 100, bottom: 10 }}
-          yAccessor={({ item }) => item}
-          xAccessor={({ index }) => index}
-          animate
-          showGrid
-          ticks={2}
-          curve={shape.curveNatural}
+        <Svg
+          width={width}
+          height={height/ 2}
+          style={{backgroundColor: COLORS.BG}}
+          {...this._panResponder.panHandlers}
         >
-          <Grid />
-          <Shadow />
-          <Tooltip />
-        </LineChart>
-
+          <Path
+            d={paths.path}
+            stroke={COLORS.BASE_LINE}
+            fill={'transparent'}
+            strokeWidth={5}
+            strokeCap="square" // or round(default)/square
+            strokeJoin="round" // or miter/round(default)
+          />
+          <Tooltip {...extraProps} />
+        </Svg>
       </View>
     );
   }
